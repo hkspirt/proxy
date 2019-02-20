f1033de41dbddc4d39da14b78ee54f45d33c85c9
徐建华 <xujianhua@lmdgame.com>
2019-02-20 17:30:41 +0800

    优化

--- internal/proxyer.go
+++ internal/proxyer.go
@@ -7,26 +7,27 @@
 package internal
 
 import (
+	"context"
 	"github.com/hkspirt/proxy/util"
 	"io/ioutil"
+	"math/rand"
+	"net"
 	"net/http"
 	"net/url"
 	"regexp"
 	"strings"
-	"sync"
 	"time"
 )
 
 type IProxy interface {
 	GetOne() string
+	Add(string)
+	Has(string) bool
 }
 
 type IProxyer interface {
 	Init()
 	Start(IProxy, IProxyer, <-chan bool)
-	GetOne() string
-	UseFailed(string)
-
 	load()
 	check(string) bool
 }
@@ -35,30 +36,29 @@ const (
 	AliveCheckTimeOut = 10 * time.Second         //单个代理检测超时
 	ProxyGetTimeOut   = 30 * time.Second         //单个代理获取超时
 	AliveCheckUrl     = "http://httpbin.org/get" //代理检测地址
-	LoadSuccessTimer  = time.Minute * 30         //重新获取一次数据间隔-上一次获取成功
-	LoadFailedTimer   = time.Second * 15         //重新获取一次数据间隔-上一次获取失败
+	LoadSuccessTimer  = time.Minute * 10         //重新获取一次数据间隔-上一次获取成功
+	LoadFailedTimer   = time.Second * 10         //重新获取一次数据间隔-上一次获取失败
 )
 
 type Proxyer struct {
-	Url       string
-	NeedProxy bool
-	proxy     IProxy //通过代理去获取
-	cookies   string
+	Url     string
+	proxy   IProxy //通过代理去获取
+	cookies string
 
 	regexp *regexp.Regexp
 	addrs  []string
-	canuse sync.Map
 }
 
 func (self *Proxyer) Start(pm IProxy, p IProxyer, cstop <-chan bool) {
 	self.proxy = pm
 	for {
 		p.load()
-		self.canuse = sync.Map{}
 		for _, addr := range self.addrs {
-			if p.check(addr) {
-				util.LogInfo("proxyer:%s proxy url:%s check success", self.Url, addr)
-				self.canuse.Store(addr, time.Now().Unix())
+			if pm.Has(addr) {
+				continue
+			}
+			if self.check(addr) {
+				pm.Add(addr)
 			}
 		}
 		if len(self.addrs) > 0 {
@@ -70,23 +70,37 @@ func (self *Proxyer) Start(pm IProxy, p IProxyer, cstop <-chan bool) {
 }
 
 func (self *Proxyer) newHttpClient() (*http.Client, string, string) {
-	var px func(*http.Request) (*url.URL, error) = nil
-	paddr := self.proxy.GetOne()
-	if paddr != "" && self.NeedProxy {
-		px = func(*http.Request) (*url.URL, error) { return url.Parse(paddr) }
+	var px func(*http.Request) (*url.URL, error)
+	paddr := ""
+	if rand.Intn(10)%2 == 0 { //随机选择是否用代理
+		paddr = self.proxy.GetOne()
 	}
-	if px == nil && self.NeedProxy {
-		return nil, "", ""
+
+	if paddr != "" {
+		px = func(*http.Request) (*url.URL, error) { return url.Parse(paddr) }
 	}
 
 	client := &http.Client{
-		Transport: &http.Transport{Proxy: px},
-		Timeout:   ProxyGetTimeOut,
+		Transport: &http.Transport{
+			Proxy: px,
+			DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
+				conn, err := net.DialTimeout(network, addr, time.Second*5)
+				if err != nil {
+					return nil, err
+				}
+				conn.SetDeadline(time.Now().Add(time.Second * 15))
+				return conn, nil
+			},
+			TLSHandshakeTimeout:   time.Second * 5,
+			ResponseHeaderTimeout: time.Second * 10,
+			IdleConnTimeout:       time.Second * 5,
+		},
+		Timeout: ProxyGetTimeOut,
 	}
-	if strings.HasPrefix(paddr, "http") {
-		return client, "http://" + self.Url, paddr
-	} else {
+	if strings.HasPrefix(paddr, "https") {
 		return client, "https://" + self.Url, paddr
+	} else {
+		return client, "http://" + self.Url, paddr
 	}
 }
 
@@ -104,6 +118,12 @@ func (self *Proxyer) httpGet() []byte {
 	}
 	req.Header.Set("Cookie", self.cookies)
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36")
+	req.Header.Set("Connection", "keep-alive")
+	req.Header.Set("Cache-Control", "max-age=0")
+	req.Header.Set("Upgrade-Insecure-Requests", "1")
+	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.8")
+	//req.Header.Set("Accept-Encoding", "gzip, deflate, sdch")
 
 	resp, err := client.Do(req)
 	if err != nil {
@@ -122,19 +142,6 @@ func (self *Proxyer) httpGet() []byte {
 		return []byte{}
 	}
 	return data
-}
-
-func (self *Proxyer) GetOne() string {
-	addr := ""
-	self.canuse.Range(func(key, value interface{}) bool {
-		addr = key.(string)
-		return false
-	})
-	return addr
-}
-
-func (self *Proxyer) UseFailed(addr string) {
-	self.canuse.Delete(addr)
 }
 
 func (self *Proxyer) check(addr string) bool {
--- proxy.go
+++ proxy.go
@@ -9,10 +9,18 @@ package proxy
 import (
 	"github.com/hkspirt/proxy/internal"
 	"github.com/hkspirt/proxy/util"
+	"math/rand"
+	"sync"
+	"sync/atomic"
+	"time"
 )
 
 var (
-	Proxy = &proxy{proxies: map[string]internal.IProxyer{}}
+	Proxy = &proxy{snatchs: map[string]internal.IProxyer{}, canuse: map[string]int{}, usefailed: map[string]int{}}
+)
+
+const (
+	FailedMaxTime = 50 //代理连续失败的最大次数(超过该次数则从池子中移除)
 )
 
 func init() {
@@ -20,39 +28,114 @@ func init() {
 }
 
 type proxy struct {
-	proxies map[string]internal.IProxyer
+	sync.RWMutex
+	snatchs   map[string]internal.IProxyer
+	canuse    map[string]int
+	usefailed map[string]int
+	running   int32
+}
+
+func (self *proxy) Run() {
+	Proxy.start()
+}
+
+func (self *proxy) Add(addr string) {
+	self.Lock()
+	defer self.Unlock()
+
+	_, a1 := self.canuse[addr]
+	_, a2 := self.usefailed[addr]
+	if !a1 && !a2 {
+		self.canuse[addr] = 1
+	}
+}
+
+func (self *proxy) Has(addr string) bool {
+	self.RLock()
+	defer self.RUnlock()
+
+	_, a1 := self.canuse[addr]
+	_, a2 := self.usefailed[addr]
+	return a1 || a2
+}
+
+func (self *proxy) Get() string {
+	return self.GetOne()
 }
 
 func (self *proxy) GetOne() string {
-	for _, p := range self.proxies {
-		addr := p.GetOne()
-		if addr != "" {
-			return addr
-		}
+	self.RLock()
+	defer self.RUnlock()
+
+	for addr, _ := range self.canuse {
+		return addr
+	}
+	for addr, _ := range self.usefailed {
+		util.LogWarn("no can use proxy")
+		return addr
 	}
 	return ""
 }
 
+func (self *proxy) UseSuccess(addr string) {
+	self.Lock()
+	defer self.Unlock()
+
+	last := self.canuse[addr]
+	last += 128
+	self.canuse[addr] = last
+	delete(self.usefailed, addr)
+}
+
 func (self *proxy) UseFailed(addr string) {
-	for _, p := range self.proxies {
-		p.UseFailed(addr)
+	self.Lock()
+	defer self.Unlock()
+
+	last, ok := self.canuse[addr]
+	if !ok {
+		self.usefailed[addr] -= 1
+		return
 	}
-	util.LogWarn("proxy use failed:%s", addr)
+
+	if last > 0 {
+		last /= 2
+	} else {
+		last -= 1
+	}
+
+	if last < FailedMaxTime {
+		delete(self.canuse, addr)
+		self.usefailed[addr] = last
+	} else {
+		self.canuse[addr] = last
+		delete(self.usefailed, addr)
+	}
 }
 
 func (self *proxy) start() {
-	self.proxies["Kuaidaili_intr"] = &internal.Kuaidaili{Proxyer: internal.Proxyer{Url: "www.kuaidaili.com/free/intr/", NeedProxy: false}}                      //ok
-	self.proxies["Kuaidaili_inha"] = &internal.Kuaidaili{Proxyer: internal.Proxyer{Url: "www.kuaidaili.com/free/inha/", NeedProxy: false}}                      //ok
-	self.proxies["Data5u"] = &internal.Data5u{Proxyer: internal.Proxyer{Url: "www.data5u.com/free/index.shtml", NeedProxy: false}}                              //ok
-	self.proxies["Ip66"] = &internal.Ip66{Proxyer: internal.Proxyer{Url: "www.66ip.cn/mo.php?tqsl=50", NeedProxy: true}}                                        //ok
-	self.proxies["Proxylistplus"] = &internal.Proxylistplus{Proxyer: internal.Proxyer{Url: "list.proxylistplus.com/Fresh-HTTP-Proxy-List-1", NeedProxy: false}} //ok
-	self.proxies["Xicidaili_nn"] = &internal.Xicidaili{Proxyer: internal.Proxyer{Url: "www.xicidaili.com/nn", NeedProxy: false}}                                //ok
-	self.proxies["Xicidaili_nt"] = &internal.Xicidaili{Proxyer: internal.Proxyer{Url: "www.xicidaili.com/nt", NeedProxy: false}}                                //ok
+	if !atomic.CompareAndSwapInt32(&self.running, 0, 1) {
+		return
+	}
 
-	for _, px := range self.proxies {
-		tmp := px
-		tmp.Init()
+	self.snatchs["Kuaidaili_intr"] = &internal.Kuaidaili{Proxyer: internal.Proxyer{Url: "www.kuaidaili.com/free/intr/"}}                      //ok
+	self.snatchs["Kuaidaili_inha"] = &internal.Kuaidaili{Proxyer: internal.Proxyer{Url: "www.kuaidaili.com/free/inha/"}}                      //ok
+	self.snatchs["Data5u"] = &internal.Data5u{Proxyer: internal.Proxyer{Url: "www.data5u.com"}}                                               //ok
+	self.snatchs["Data5u_in"] = &internal.Data5u{Proxyer: internal.Proxyer{Url: "www.data5u.com/free/index.shtml"}}                           //ok
+	self.snatchs["Data5u_gngn"] = &internal.Data5u{Proxyer: internal.Proxyer{Url: "www.data5u.com/free/gngn/index.shtml"}}                    //ok
+	self.snatchs["Data5u_gnpt"] = &internal.Data5u{Proxyer: internal.Proxyer{Url: "www.data5u.com/free/gnpt/index.shtml"}}                    //ok
+	self.snatchs["Data5u_gwgn"] = &internal.Data5u{Proxyer: internal.Proxyer{Url: "www.data5u.com/free/gwgn/index.shtml"}}                    //ok
+	self.snatchs["Data5u_gwpt"] = &internal.Data5u{Proxyer: internal.Proxyer{Url: "www.data5u.com/free/gwpt/index.shtml"}}                    //ok
+	self.snatchs["Ip66_mo"] = &internal.Ip66{Proxyer: internal.Proxyer{Url: "www.66ip.cn/mo.php?tqsl=100"}}                                   //ok
+	self.snatchs["Ip66_nmtq"] = &internal.Ip66{Proxyer: internal.Proxyer{Url: "www.66ip.cn/nmtq.php?getnum=100"}}                             //ok
+	self.snatchs["Proxylistplus"] = &internal.Proxylistplus{Proxyer: internal.Proxyer{Url: "list.proxylistplus.com/Fresh-HTTP-Proxy-List-1"}} //ok
+	self.snatchs["Xicidaili_nn"] = &internal.Xicidaili{Proxyer: internal.Proxyer{Url: "www.xicidaili.com/nn"}}                                //ok
+	self.snatchs["Xicidaili_nt"] = &internal.Xicidaili{Proxyer: internal.Proxyer{Url: "www.xicidaili.com/nt"}}                                //ok
+
+	for _, snatch := range self.snatchs {
+		snatch.Init()
+		tmp := snatch
 		util.GoChan(func(cstop <-chan bool) {
+			time.Sleep(time.Millisecond * time.Duration(rand.Intn(5000)))
 			tmp.Start(self, tmp, cstop)
 		})
 	}
